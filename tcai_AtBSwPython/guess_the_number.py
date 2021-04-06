1ad8ff88a918ebfb2bfc40af8ff7bf738c1e54ae
CailleauThierry <thierrycailleau@yahoo.fr>
2021-04-07 01:43:58 +0200

    some quick changes

--- tcai_AtBSwPython/guess_the_number.py
+++ tcai_AtBSwPython/guess_the_number.py
@@ -8,7 +8,7 @@ import random
 print('Hello. What is your name?')
 name = input()
 secretNumber = random.randint(1, 20)
-print('Well, ' + name + 'I am thinking of a number between 1 and 20.')
+print(f"Well, {name} I am thinking of a number between 1 and 20.")
 # Comment-out the "DEGUG:" line below before submitting the production code!
 print('DEBUG: Secret number is ' + str(secretNumber))
 
@@ -26,7 +26,7 @@ for guessesTaken in range (1, 7):
         break
 
 if guessedNumber == secretNumber:
-    print('Good job, ' + name + "! You guessed my number in " + str(guessesTaken) + " guesses!")
+    print(f"Good job, {name}! You guessed my number in {str(guessesTaken)} guesses! )
 else:
-    print('Nope ' + name + " the number i was thinking of was " + str(secretNumber) + " !")
+    print("Nope " + name + " the number i was thinking of was " + str(secretNumber) + "!")
 
