e576c5c4e274db3f46ed0c4d559084b5c9b199bf
CailleauThierry <thierrycailleau@yahoo.fr>
2021-05-09 02:33:47 +0200

    Completed the Regex section

--- tcai_AtBSwPython/_nbdev.py
+++ tcai_AtBSwPython/_nbdev.py
@@ -6,13 +6,23 @@ index = {"display_fraction": "04_functions.ipynb",
          "name": "05_guess_the_number.ipynb",
          "secretNumber": "05_guess_the_number.ipynb",
          "spam": "06_lists.ipynb",
-         "hello_world": "09_Python_command_line.ipynb"}
+         "hello_world": "09_Python_command_line.ipynb",
+         "phoneRegEx": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "emailRegEx": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "text": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "listOfAllPhoneNumbers": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "allPhoneNumbers": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "allEmailAddresses": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "results": "10_1_Python_Phone_and_email_scrapper.ipynb",
+         "isPhoneNumber": "10_Python_Regular_Expresssions.ipynb"}
 
 modules = ["functions.py",
            "guess_the_number.py",
            "lists.py",
            "python_cli_arguments.py",
-           "python_from_cli.py"]
+           "python_from_cli.py",
+           "Phone_and_email_scrapper.py",
+           "Regular_Expresssions.py"]
 
 doc_url = "https://CailleauThierry.github.io/tcai_AtBSwPython/"
 
